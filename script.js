cf7f50d13fdd8271ca1eef99f579bf919473ebe3
Jack Martin <jackmartin.dev@gmail.com>
2023-11-13 17:46:48 +0900

    Add axios methods

--- script.js
+++ script.js
@@ -1,2 +1,47 @@
+import axios from "axios"
 import "bootstrap"
 import "bootstrap/dist/css/bootstrap.min.css"
+
+const queryParamsContainer = document.querySelector("[data-query-params]")
+const queryHeadersContainer = document.querySelector("[data-request-headers]")
+const keyValueTemplate = document.querySelector("[data-key-value-template]")
+const form = document.querySelector("[data-form]")
+
+form.addEventListener("submit", (e)=>{
+    e.preventDefault();
+
+    axios({
+        url: document.querySelector("[data-url]").value,
+        method: document.querySelector("[data-method]").value,
+        params: keyValuePairsToObjects(queryParamsContainer),
+        headers: keyValuePairsToObjects(queryHeadersContainer),
+    }).then(res => console.log(res.data))
+})
+
+document.querySelector("[data-add-query-param-btn]").addEventListener("click", () =>{
+    queryParamsContainer.append(createKeyValuePair())
+})
+document.querySelector("[data-add-request-header-btn]").addEventListener("click", () =>{
+    queryHeadersContainer.append(createKeyValuePair())
+})
+
+queryParamsContainer.append(createKeyValuePair())
+queryHeadersContainer.append(createKeyValuePair())
+
+function createKeyValuePair() {
+    const element = keyValueTemplate.content.cloneNode(true);
+    element.querySelector("[data-remove-btn]").addEventListener("click", (e) =>{
+        e.target.closest("[data-key-value-pair]").remove()
+    })
+    return element
+}
+
+function keyValuePairsToObjects(container) {
+    const pairs = container.querySelectorAll("[data-key-value-pair]")
+    return [...pairs].reduce((data,pair) => {
+        const key = pair.querySelector("[data-key]").value;
+        const value = pair.querySelector("[data-value]").value;
+        if(key ==="") return data;
+        return {...data, [key]:value}
+    }, {})
+}
